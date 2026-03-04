f42a06e9c46967f1e5d7e56ac6a6537215802a06
pushpa-sree <vankadaripushpa123@gmail.com>
2026-03-04 22:39:12 +0530

    Day 04 Recursion q1 and q2 are added

---
+++ Recursions/Recursions_day04_q1.java
@@ -0,0 +1,37 @@
+import java.util.*; 
+public class Recursions_day04_q1 {
+  // day04
+  //recursion
+  //https://leetcode.com/problems/subsets/
+     public static  void GenerateSubsets(List<List<Integer>> result , List <Integer> temp, int[] nums , int current){
+        if(current >= nums.length){
+            result.add(new ArrayList<>(temp));
+            return ; 
+        }
+        ;
+        temp.add(nums[current]);
+        GenerateSubsets(result , temp , nums , current+1);
+        temp.remove(temp.size()-1);
+        GenerateSubsets(result , temp , nums , current+1);
+    }
+    public static List<List<Integer>> subsets(int[] nums) {
+        int n = nums.length - 1; 
+        List<List<Integer>> result = new ArrayList<>();
+        List <Integer> temp = new ArrayList<>();
+        GenerateSubsets(result , temp ,  nums , 0);
+        return result; 
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0 ; i < n ; i++){
+            arr[i] = sc.nextInt();
+        }
+        List<List<Integer>> res = subsets(arr);
+        for (List<Integer> i : res) {
+            System.out.println(i);
+        }
+    }
+    
+}
---
+++ Recursions/Recursions_day04_q2.java
@@ -0,0 +1,50 @@
+import java.util.*; 
+class Recursions_day04_q2{
+
+    // day 04
+    //Recursions
+    //https://leetcode.com/problems/permutations/
+      public static void permutation(int[] nums, List<Integer> current,
+                                   boolean[] used, List<List<Integer>> result) {
+
+        if (current.size() == nums.length) {
+            result.add(new ArrayList<>(current));
+            return;   
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+
+            if (used[i]) continue;
+
+            used[i] = true;
+            current.add(nums[i]);
+
+            permutation(nums, current, used, result);
+
+            current.remove(current.size() - 1);
+            used[i] = false;
+        }
+    }
+
+    public static List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        boolean[] used = new boolean[nums.length];
+
+        permutation(nums, new ArrayList<>(), used, result);
+
+        return result;
+    }
+         public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+             int n = sc.nextInt();
+             int[] nums = new int[n];
+             for(int i = 0 ; i < n ; i++){
+                nums[i] = sc.nextInt();
+             }
+             List<List<Integer>> res = permute(nums);
+            for (List<Integer> i : res) {
+            System.out.println(i);
+              }
+
+         }
+}
